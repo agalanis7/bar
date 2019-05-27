5de42f0d48e71b394bc0dfaa59e02cf921fc896d
Andreas Galanis <agalanis7@gmail.com>
2019-05-27 14:51:46 -0400

    final final boilerplate

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+const path = require("path")
 const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/userRouter');
 const userTask = require('./routers/taskRouter');
 
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public/");
+app.use(express.static(publicDirectoryPath))
 
 app.use(express.json());
 //import routes from routers folder
